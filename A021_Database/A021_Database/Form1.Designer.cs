9b610905ea6010ed0f1c46dd72a353a8a7a670cf
JK0925 <51144126+JK0925@users.noreply.github.com>
2019-05-29 11:30:59 +0900

    Add files via upload

---
+++ A021_Database/A021_Database/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace A021_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtSld = new System.Windows.Forms.TextBox();
+            this.txtSName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("굴림", 12F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(21, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(450, 196);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(28, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 11F);
+            this.label2.Location = new System.Drawing.Point(28, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "학번";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 11F);
+            this.label3.Location = new System.Drawing.Point(28, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "이름";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 11F);
+            this.label4.Location = new System.Drawing.Point(28, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "전화번호";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("굴림", 11F);
+            this.txtID.Location = new System.Drawing.Point(113, 269);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(130, 24);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtSld
+            // 
+            this.txtSld.Font = new System.Drawing.Font("굴림", 11F);
+            this.txtSld.Location = new System.Drawing.Point(113, 309);
+            this.txtSld.Name = "txtSld";
+            this.txtSld.Size = new System.Drawing.Size(130, 24);
+            this.txtSld.TabIndex = 6;
+            // 
+            // txtSName
+            // 
+            this.txtSName.Font = new System.Drawing.Font("굴림", 11F);
+            this.txtSName.Location = new System.Drawing.Point(113, 351);
+            this.txtSName.Name = "txtSName";
+            this.txtSName.Size = new System.Drawing.Size(130, 24);
+            this.txtSName.TabIndex = 7;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("굴림", 11F);
+            this.txtPhone.Location = new System.Drawing.Point(113, 393);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(130, 24);
+            this.txtPhone.TabIndex = 8;
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnViewAll.Location = new System.Drawing.Point(287, 268);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(75, 23);
+            this.btnViewAll.TabIndex = 9;
+            this.btnViewAll.Text = "View all";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnInsert.Location = new System.Drawing.Point(287, 308);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "추가";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnSearch.Location = new System.Drawing.Point(287, 350);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnClear.Location = new System.Drawing.Point(287, 392);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnDelete.Location = new System.Drawing.Point(382, 308);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnUpdate.Location = new System.Drawing.Point(382, 350);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("굴림", 11F);
+            this.btnExit.Location = new System.Drawing.Point(382, 392);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "종료";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 451);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSName);
+            this.Controls.Add(this.txtSld);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtSld;
+        private System.Windows.Forms.TextBox txtSName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ A021_Database/A021_Database/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace A021_Database
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = null;
+        OleDbCommand comm = null;
+        OleDbDataReader reader = null;
+
+        string connString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\konynag\Desktop\18615031 우제균\A021_Database\A021_Database\StudentTable.accdb;
+Persist Security Info=False;";
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayStudent();
+        }
+
+        private void DisplayStudent()
+        {
+            if (conn == null)
+            {
+                conn = new OleDbConnection(connString);
+                conn.Open();
+            }
+            //명령어: 모든 레코드를 가져와라
+            //명령어는 SQL로 만든다
+            string sql = "SELECT * FROM StudentTable";
+            comm = new OleDbCommand(sql, conn);
+
+            //명령어를 실행
+            reader = comm.ExecuteReader();
+
+            // DB에서 읽어오는 여러개의 레코드
+            while(reader.Read())
+            {
+                string x = "";
+                x += reader["ID"] + "\t";
+                x += reader["SID"] + "\t";
+                x += reader["SName"] + "\t";
+                x += reader["Phone"];
+                listBox1.Items.Add(x);
+            }
+            reader.Close();
+            conn.Close();
+            conn = null;
+        }
+    }
+}
