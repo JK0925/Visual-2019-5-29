3def3f145a2fcce739e2b3ddca6ecbb32888724b
JK0925 <51144126+JK0925@users.noreply.github.com>
2019-05-29 11:36:18 +0900

    Add files via upload

---
+++ A026_DigitalClockWF/DispatcherTimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A026_DigitalClockWF
+{
+    internal class DispatcherTimer
+    {
+        public TimeSpan Interval { get; internal set; }
+        public int Tick { get; internal set; }
+
+        internal void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ A026_DigitalClockWF/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A026_DigitalClockWF
+{
+    public partial class Form1 : Form
+    {
+        private object dClock;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Timer_Tick { get; private set; }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+    }
+}
